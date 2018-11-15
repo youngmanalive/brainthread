a40103ae229c9073e17bbd6de7d39fa38706e21f
Nate Harris <nathanharris.sf@gmail.com>
2018-11-14 21:01:16 -0800

    Add store and token handling to entry

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
+
 import Root from "./js/components/root";
 
-const App = () => (
-  <Provider>
+const App = ({ store }) => (
+  <Provider store={store}>
     <HashRouter>
       <Root />
     </HashRouter>
