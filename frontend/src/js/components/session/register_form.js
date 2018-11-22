58eceb26f0e177d4e08b056a5b0e8bc05b25197e
Nate Harris <nathanharris.sf@gmail.com>
2018-11-21 23:53:16 -0800

    Modify register form to check username availability

--- frontend/src/js/components/session/register_form.js
+++ frontend/src/js/components/session/register_form.js
@@ -1,18 +1,24 @@
 import React from "react";
-import CheckUsername from './check_username';
 
 class RegisterForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "",
-      lastName: "",
-      username: "",
-      email: "",
-      password: "",
-      password2: "",
+      user: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        email: "",
+        password: "",
+        password2: "",
+      },
+      checkingName: false,
+      validInput: true,
+      next: false
     }
 
+    this.typingTimeout = null;
+    this.handleUsername = this.handleUsername.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -20,58 +26,145 @@ class RegisterForm extends React.Component {
     if (Object.keys(this.props.errors).length) this.props.clearErrors();
   }
 
-  update(field) {
-    return e => this.setState({ [field]: e.currentTarget.value });
+  registerUsername() {
+    const buttonDisabled = (this.state.checkingName) || !(
+      this.props.validUsername &&
+      this.state.validInput &&
+      this.state.user.username.length > 3
+    );
+
+    const style = {display: "inline", marginRight: "15px"};
+
+    return (
+      <>
+        <h2>Create your username</h2>
+        <input
+          style={style}
+          type="text"
+          onChange={this.handleUpdate('username')}
+          onKeyUp={this.handleUsername}
+          value={this.state.user.username} 
+          placeholder="Username" />
+        <h4 style={style}>{this.usernameStatus()}</h4>
+        <br/>
+        <br/>
+        <button 
+          onClick={() => this.setState({ next: true })}
+          disabled={buttonDisabled}>
+          Next
+        </button>
+      </>
+    )
+  }
+
+  usernameStatus() {
+    if (this.state.checkingName) {
+      return <i className="fa fa-spinner fa-pulse" />;
+    } else if (this.state.user.username.length < 4) {
+      return null;
+    } else if (!this.state.validInput) {
+      return <i className="fa fa-times" style={{color: "red"}}> Invalid characters</i>;
+    } else if (!this.props.validUsername) {
+      return <i className="fa fa-times" style={{color: "red"}} />
+    } else {
+      return <i className="fa fa-check" style={{color: "green"}} />;
+    }
+  }
+
+  handleUpdate(field) {
+    if (field === "username") {
+      return e => this.setState({
+        user: {
+          ...this.state.user,
+          [field]: e.currentTarget.value.toLowerCase()
+        },
+        checkingName: e.currentTarget.value.length > 3,
+        validInput: true
+      })
+    } else {
+      return e => this.setState({ user: {
+        ...this.state.user,
+        [field]: e.currentTarget.value
+      }});
+    }
+  }
+
+  handleUsername() {
+    if (this.typingTimeout) clearTimeout(this.typingTimeout);
+
+    if (this.state.user.username.length && !/^[a-z0-9_]+$/.test(this.state.user.username)) {
+      return this.setState({ validInput: false, checkingName: false });
+    }
+
+    if (this.state.checkingName) {
+      this.typingTimeout = setTimeout(() => {
+        new Promise(() => this.props.checkUsername(this.state.user.username))
+          .then(setTimeout(() => (
+            this.setState({ checkingName: false, validInput: true })
+          ), 300));
+      }, 700);
+    }
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
+    const user = Object.assign({}, this.state.user);
     this.props.register(user);
   }
 
-  render() {
+  registerUserInfo() {
     return (
       <>
-        <CheckUsername/>
+        <h2>Almost done</h2>
+        <h4>{this.state.user.username}</h4>
         <form onSubmit={this.handleSubmit}>
-          <h1>Sign up!</h1>
           <input
             type="text" 
-            onChange={this.update("firstName")} 
-            value={this.state.firstName}
+            onChange={this.handleUpdate("firstName")} 
+            value={this.state.user.firstName}
             placeholder="First Name" />
           <input
             type="text" 
-            onChange={this.update("lastName")} 
-            value={this.state.lastName}
+            onChange={this.handleUpdate("lastName")} 
+            value={this.state.user.lastName}
             placeholder="Last Name" />
           <input
-            type="text" 
-            onChange={this.update("username")} 
-            value={this.state.username}
-            placeholder="Username" />
-          <input
-            type="text" 
-            onChange={this.update("email")} 
-            value={this.state.email}
+            type="email" 
+            onChange={this.handleUpdate("email")} 
+            value={this.state.user.email}
             placeholder="Email" />
           <input
             type="text" 
-            onChange={this.update("password")} 
-            value={this.state.password}
+            onChange={this.handleUpdate("password")} 
+            value={this.state.user.password}
             placeholder="Password" />
           <input
             type="text" 
-            onChange={this.update("password2")} 
-            value={this.state.password2}
+            onChange={this.handleUpdate("password2")} 
+            value={this.state.user.password2}
             placeholder="Confirm Password" />
           <input
             type="submit"
-            value="Submit" />
+            value="Register" />
         </form>
+        <button onClick={() => this.setState({ next: false })}>
+          Go back
+        </button>
       </>
     )
+  }
+
+  render() {
+    const form = this.state.next
+      ? (this.registerUserInfo())
+      : (this.registerUsername());
+  
+    return (
+      <>
+        <h1>Sign Up!</h1>
+        {form}
+      </>
+    );
   }
 }
 
--- frontend/src/js/components/session/register_form_container.js
+++ frontend/src/js/components/session/register_form_container.js
@@ -1,16 +1,18 @@
 import { connect } from "react-redux";
 
 import RegisterForm from "./register_form";
-import { registerUser } from "../../util/user_api_util";
+import { registerUser, checkUsername } from "../../util/user_api_util";
 import { clearErrors } from "../../actions/error_actions";
 
 const mapStateToProps = state => ({
-  errors: state.errors.session
+  errors: state.errors.session,
+  validUsername: state.session.username
 });
 
 const mapDispatchToProps = dispatch => ({
   register: user => dispatch(registerUser(user)),
-  clearErrors: () => dispatch(clearErrors())
+  clearErrors: () => dispatch(clearErrors()),
+  checkUsername: username => dispatch(checkUsername(username))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RegisterForm);
