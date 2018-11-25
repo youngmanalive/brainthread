3651ff609fb3d95971c86a039f3f864eab94cf10
Nate Harris <nathanharris.sf@gmail.com>
2018-11-24 18:09:25 -0800

    Move backend into server directory

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+const favicon = require("serve-favicon");
+
+// Port
+const port = process.env.PORT || 5000;
+
+// Routes
+const users = require("./routes/api/users");
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+// Mongoose
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+
+app.use("/api/users", users);
+
+app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ server/msc.js
@@ -0,0 +1,5 @@
+// "start": "node app.js",
+// "server": "nodemon app.js",
+// "frontend-install": "npm install --prefix frontend",
+// "frontend": "npm start --prefix frontend",
+// "dev": "concurrently \"npm run server\" \"npm run frontend\""
