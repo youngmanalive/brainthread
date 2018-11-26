0cd76ac93bf70153a0d0265b605af820ab04e171
Nate Harris <nathanharris.sf@gmail.com>
2018-11-25 23:50:39 -0800

    Add get request for testing protected routes

--- client/src/js/components/home/test.js
+++ client/src/js/components/home/test.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { getCurrentUser } from "../../util/user_api_util";
 
 class Test extends React.Component {
   render() {
     return (
       <>
         <h1>You made it, {this.props.username}!</h1>
+        <p>
+          Clicking <button 
+                      onClick={() => this.props.getCurrentUser()}
+                      >HERE</button> will
+          make an authorized get request. See console.
+        </p>
         <Link to="/home">Home</Link>
       </>
     )
@@ -14,5 +21,6 @@ class Test extends React.Component {
 }
 
 const msp = state => ({ username: state.session.username });
+const mdp = dispatch => ({ getCurrentUser: () => dispatch(getCurrentUser())});
 
-export default connect(msp)(Test);
+export default connect(msp, mdp)(Test);
