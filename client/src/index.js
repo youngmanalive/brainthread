6d4be89045f4a4e0923d4ed53be1ade697032a0d
Nate Harris <nathanharris.sf@gmail.com>
2018-11-25 23:42:47 -0800

    Refactor session handling

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import jwt_decode from "jwt-decode";
 import configureStore from "./js/store/store";
-import * as APIUtil from "./js/util/user_api_util";
+import { setSession } from "./js/util/user_api_util";
 
 import "font-awesome/css/font-awesome.min.css";
 import "./css/styles.scss";
@@ -13,19 +12,7 @@ import App from "./app";
 document.addEventListener("DOMContentLoaded", () => {
   let store = configureStore();
 
-  if (localStorage.jwtToken) {
-    APIUtil.setAuthToken(localStorage.jwtToken);
-    const decoded = jwt_decode(localStorage.jwtToken);
-    store.dispatch(APIUtil.setCurrentUser(decoded));
-
-    const currentTime = Date.now() / 1000;
-    if (decoded.exp < currentTime) {
-      store.dispatch(APIUtil.logoutUser());
-      window.location.href = "/login";
-    }
-  }
-
-  window.getState = store.getState;
+  if (localStorage.jwtToken) store.dispatch(setSession());
 
   ReactDOM.render(<App store={store}/>, document.getElementById("root"));
 });
