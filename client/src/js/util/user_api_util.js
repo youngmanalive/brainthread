e856dfadc7e26a400467509b4cfd2831941181af
Nate Harris <nathanharris.sf@gmail.com>
2018-12-07 21:07:58 -0800

    Refactoring and jwt token handling

--- client/src/js/util/user_api_util.js
+++ client/src/js/util/user_api_util.js
@@ -3,8 +3,13 @@ import jwt_decode from "jwt-decode";
 
 // SET AUTHORIZATION TOKEN
 const setAuthToken = token => {
-  axios.defaults.headers.common.Authorization = token ? token : null;
+  axios.defaults.headers.common.Authorization = token;
 };
+
+const updateToken = token => {
+  setAuthToken(token)
+  localStorage.setItem("jwtToken", token);
+}
 
 
 // REGISTER USER
@@ -14,11 +19,10 @@ export const createUser = user =>
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
       setAuthToken(token);
-      const decoded = jwt_decode(token);
-      return decoded
+      return jwt_decode(token);
     })
     .catch(err => {
-      throw err.response.data 
+      throw err.response.data;
     });
 
 
@@ -29,26 +33,30 @@ export const createSession = credentials =>
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
       setAuthToken(token);
-      const decoded = jwt_decode(token);
-      return decoded;
+      return jwt_decode(token);
     })
-    .catch( err => {
+    .catch(err => {
       throw err.response.data;
     });
 
 
 // LOGOUT USER
-export const destroySession = () => new Promise(resolve => {
-  localStorage.removeItem("jwtToken");
-  setAuthToken();
-  resolve();
-})
+export const destroySession = () =>
+  new Promise(resolve => {
+    localStorage.removeItem("jwtToken");
+    setAuthToken(null);
+    resolve();
+  });
 
 
 // GET CURRENT USER - jwt testing route
 export const getCurrentUser = () =>
   axios.get("/api/users/current")
-    .then(res => console.log(res.data))
+    .then(res => {
+      if (res.data.token) updateToken(res.data.token);
+      console.log("Current User Id:", res.data.id);
+      if (res.data.token) console.log("Token Updated!");
+    })
     .catch(err => console.log(err));
 
 
@@ -62,9 +70,9 @@ export const getUsername = name =>
 export const setSession = () => {
   const decoded = jwt_decode(localStorage.jwtToken);
   const expired = decoded.exp < (Date.now() / 1000);
-  
+
   if (expired) localStorage.removeItem("jwtToken");
-  
+
   setAuthToken(expired ? null : localStorage.jwtToken);
   return Promise.resolve(expired ? {} : decoded);
 }
