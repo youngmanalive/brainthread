bf2d3f3aaf3b93fe1d61df2b358e22bc4ffb67b0
Nate Harris <nathanharris.sf@gmail.com>
2018-12-07 21:10:51 -0800

    JWT exp handling

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,16 +4,37 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("./keys");
 
+const jwt = require("jsonwebtoken");
+
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: keys.secret
 };
 
+const updateToken = payload => 
+  jwt.sign(payload, keys.secret, { expiresIn: 4500 });
+
 module.exports = passport => {
   passport.use(
     new JwtStrategy(options, (payload, done) => {
+      let refreshToken = false;
+
+      if (payload.clientExp < (Date.now() / 1000)) {
+        const newPayload = {
+          id: payload.id,
+          username: payload.username,
+          clientExp: (Date.now() / 1000 + 2700)
+        };
+        refreshToken = `Bearer ${updateToken(newPayload)}`;
+      }
+
+      const items = {
+        token: refreshToken,
+        id: payload.id
+      };
+
       User.findById(payload.id)
-        .then(user => done(null, (user ? user : false)))
+        .then(user => done(null, (user ? items : false)))
         .catch(err => console.log(err));
     })
   );
